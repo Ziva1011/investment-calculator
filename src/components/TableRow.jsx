0c357d8644489d081efcf94c80be1b8def225a6d
Ziva1011 <catarina.vi.pires@gmail.com>
2023-07-28 13:45:04 +0200

    formatter

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -1,14 +1,22 @@
 import "./CalculatorHeader.css"
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'EUR',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+
+});
+
 
 const TableRow =({data, investment}) => {
     console.log(investment)
     return (
           <tr>
             <td>{data.year}</td>
-            <td>{data.savingsEndOfYear}</td>
+            <td>{formatter.format(data.savingsEndOfYear)}</td>
             <td>{data.yearlyInterest}</td>
-            <td>{data.savingsEndOfYear -investment - data.yearlyContribution*data.year}</td>
-            <td>{investment + data.yearlyContribution*data.year}</td>
+            <td>{formatter.format(data.savingsEndOfYear -investment - data.yearlyContribution*data.year)}</td>
+            <td>{formatter.format(investment + data.yearlyContribution*data.year)}</td>
           </tr>
     )
 
